cf6019112cad7fcfbb2f252909572c53a47a1570
beena65 <beenashrestha61@gmail.com>
2024-07-01 21:05:42 +0545

    Added header component with profile and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Sidebar from "./components/Sidebar/Sidebar";
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import "./App.css";
 function App() {
     return (
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,42 @@
+import {AppBar, Avatar, Button, IconButton, Toolbar, Typography, Box} from "@mui/material";
+import Arrowback from "../Icons/Arrowback";
+import Erroroutline from "../Icons/Erroroutline";
+import Materialsymbol from "../Icons/Materialsymbol";
+import Infinite from "../Icons/Infinite";
+import NotificationIcon from "../Icons/NotificationIcon";
+import logo from "../../assets/profile.png";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <AppBar position="static" className="App-bar">
+            <Toolbar className="tool-bar">
+                <Box sx={{flexGrow: 1}} className="box-wrap">
+                    <IconButton edge="start">
+                        <Arrowback />
+                    </IconButton>
+                    <Typography variant="h4" className="customtypography">
+                        Amazon product price
+                    </Typography>
+
+                    <IconButton>
+                        <Erroroutline />
+                    </IconButton>
+                </Box>
+                <Box className="box-wrap">
+                    <Button className="custombutton">
+                        <Materialsymbol />
+                        <span>Credit usage: 1018 /</span>
+                        <Infinite />
+                    </Button>
+                    <IconButton>
+                        <NotificationIcon />
+                    </IconButton>
+                    <Avatar alt="profile-image" src={logo} className="profile-image" />
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Header;
