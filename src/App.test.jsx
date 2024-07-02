edd230d06d0ef41b0b070a2706769d69a3122b84
beena65 <beenashrestha61@gmail.com>
2024-07-03 00:59:45 +0545

    Refactor: remove comment section from code

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,29 +1,3 @@
-// import {describe, it, expect} from "vitest";
-// import App from "./App";
-// import {render, screen, waitFor} from "@testing-library/react";
-// import {server} from "./test/mock/server";
-// import {HttpResponse, http} from "msw";
-
-// import CustomizedTables from "./components/Content/Table";
-
-// describe("CustomizedTables Component", () => {
-//     it("testing vitest", () => {
-//         expect(true).toBeTruthy();
-//     });
-//     it("Api success scenario onload ", async () => {
-//         render(<App />);
-//     });
-
-//     it("api error scenario on load", () => {
-//         render(<App />);
-//         server.use(
-//             http.get("https://63883c2bd94a7e504095e98b.mockapi.io/fakeApiqqqqqqq", () => {
-//                 return new HttpResponse(null, {status: 401});
-//             })
-//         );
-//     });
-// });
-
 import {describe, it, expect, beforeAll, afterAll, afterEach} from "vitest";
 import {render, screen, waitFor} from "@testing-library/react";
 import {server} from "./test/mock/server";
@@ -33,8 +7,9 @@ import CustomizedTables from "./components/Content/Table";
 
 beforeAll(() => server.listen());
 afterAll(() => server.close());
-afterEach(() => server.resetHandlers());
-
+afterEach(() => {
+    server.resetHandlers();
+});
 describe("CustomizedTables Component", () => {
     it("testing vitest", () => {
         expect(true).toBeTruthy();
@@ -45,19 +20,20 @@ describe("CustomizedTables Component", () => {
 
         await waitFor(() => {
             expect(screen.getByText("product_name 1")).toBeInTheDocument();
-            expect(screen.getByText("Brand 1")).toBeInTheDocument();
-            expect(screen.getByText("Availability 1")).toBeInTheDocument();
         });
     });
 
-    it("api error scenario on load", () => {
-        render(<CustomizedTables />);
+    it("Api error scenario onload ", async () => {
         server.use(
             http.get("https://63883c2bd94a7e504095e98b.mockapi.io/fakeApiqqqqqqq", () => {
                 return new HttpResponse(null, {status: 401});
             })
         );
-        expect(screen.queryByText("Error: failed to fetch data")).toBeInTheDocument();
+        render(<CustomizedTables />);
+
+        await waitFor(() => {
+            expect(screen.getByText("Error: failed to fetch data")).toBeInTheDocument();
+        });
     });
 
     it("renders loading state correctly", async () => {
@@ -65,9 +41,12 @@ describe("CustomizedTables Component", () => {
 
         expect(screen.getByText("Loading data....")).toBeInTheDocument();
 
-        await waitFor(() => {
-            expect(screen.queryByText("Loading data....")).toBeNull();
-        });
+        await waitFor(
+            () => {
+                expect(screen.queryByText("Loading data....")).toBeNull();
+            },
+            {timeout: 5000}
+        );
     });
 
     it("renders table rows correctly on successful API fetch", async () => {
